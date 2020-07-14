f2cc3fd218d22de0d285b10a671b8aa92ef99e3d
Gabriel Castro <gabri.gcl@gmail.com>
2020-07-14 17:44:31 -0300

    Add full project

---
+++ profiles_api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from profiles_api import models
+
+admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
+
+# Register your models here.
